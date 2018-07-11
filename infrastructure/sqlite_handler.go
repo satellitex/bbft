aaeed8d2e76de6d9776ecbd817e3761bbdfe61bf
satellitex <s.a.t.e.3.ths@gmail.com>
2018-07-11 21:17:32 +0900

    impl consensusSender interface

--- infrastructure/sqlite_handler.go
+++ infrastructure/sqlite_handler.go
@@ -1,1 +1,15 @@
 package infrastructure
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/satellitex/bbft/config"
+)
+
+func NewSQLite3(config *config.Config) *sql.DB {
+	conn, err := sql.Open("sqlite3", config.Database.Name)
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
--- model/consensus.go
+++ model/consensus.go
@@ -9,3 +9,10 @@ type VoteMessage interface {
 	GetBlockHash() []byte
 	GetSignature() Signature
 }
+
+type ConsensusSender interface {
+	Propagate(ptx ProposalTx) error
+	Propose(proposal Proposal) error
+	Vote(vote VoteMessage) error
+	PreCommit(vote VoteMessage) error
+}

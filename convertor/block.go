9a29a0210b289edc9d9d3ae792059abf645c0518
satellitex <s.a.t.e.3.ths@gmail.com>
2018-07-12 21:25:21 +0900

    no use method de;

--- convertor/block.go
+++ convertor/block.go
@@ -9,16 +9,8 @@ type Block struct {
 	*bbft.Block
 }
 
-func ConvertBlock(block *bbft.Block) *Block {
-	return &Block{block}
-}
-
 type Proposal struct {
 	*bbft.Proposal
-}
-
-func ConvertProposal(proposal *bbft.Proposal) *Proposal {
-	return &Proposal{proposal}
 }
 
 type BlockHeader struct {

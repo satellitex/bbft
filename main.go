3208b45121e0bee2cd6485b09221744cf8eca58a
satellitex <s.a.t.e.3.ths@gmail.com>
2018-07-20 02:29:28 +0900

    impl server up main

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	"github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"github.com/grpc-ecosystem/go-grpc-middleware/validator"
+	"github.com/satellitex/bbft/config"
+	"github.com/satellitex/bbft/controller"
+	"github.com/satellitex/bbft/convertor"
+	"github.com/satellitex/bbft/dba"
+	"github.com/satellitex/bbft/proto"
+	"github.com/satellitex/bbft/usecase"
+	"google.golang.org/grpc"
+	"net"
+)
 
 func main() {
-	fmt.Println("bbft main")
+
+	fmt.Println("=========================== boot bbft ===========================")
+
+	conf := config.GetConfig()
+
+	l, err := net.Listen("tcp", ":"+conf.Port)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Succcess New Listen")
+
+	ps := dba.NewPeerServiceOnMemory()
+	author := convertor.NewAuthor(ps)
+
+	queue := dba.NewProposalTxQueueOnMemory(conf)
+	lock := dba.NewLockOnMemory(ps, conf)
+	pool := dba.NewReceiverPoolOnMemory(conf)
+	bc := dba.NewBlockChainOnMemory()
+	slv := convertor.NewStatelessValidator()
+	sender := convertor.NewMockConsensusSender() // WIP
+	receivChan := usecase.NewReceiveChannel(conf)
+
+	consensusReceiver := usecase.NewConsensusReceiverUsecase(queue, ps, lock, pool, bc, slv, sender, receivChan)
+	clientRceiver := usecase.NewClientGateReceiverUsecase(slv, sender)
+	fmt.Println("Success New Receivers")
+
+	s := grpc.NewServer([]grpc.ServerOption{
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_validator.UnaryServerInterceptor(),
+			grpc_ctxtags.UnaryServerInterceptor(),
+			grpc_recovery.UnaryServerInterceptor(),
+		)),
+	}...)
+	fmt.Println("Success New Server")
+
+	bbft.RegisterConsensusGateServer(s, controller.NewConsensusController(consensusReceiver, author))
+	bbft.RegisterTxGateServer(s, controller.NewClientGateController(clientRceiver, author))
+	fmt.Println("Success New Register Endpoint")
+
+	fmt.Println("Set Up!!")
+
+	if err := s.Serve(l); err != nil {
+		fmt.Printf("Failed to server grpc: %s", err.Error())
+	}
+
 }
